070a7caeda9446d4a5229bb7d5899702d1ec62a1
saul <saul@098d0599-7f2b-0410-95a5-ad30ac85393a>
2019-08-20 20:56:01 +0000

    version 19.2.17

--- CodeBase/Properties/AssemblyInfo.cs
+++ CodeBase/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("19.2.16.0")]
+[assembly: AssemblyVersion("19.2.17.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
