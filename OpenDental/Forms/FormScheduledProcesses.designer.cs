a8182ea18a3461698d213ebcc92b88d18d2d0f88
patrickc <patrickc@098d0599-7f2b-0410-95a5-ad30ac85393a>
2019-09-24 18:51:58 +0000

    B16921 - TranslationName property was not set on FormScheduledProcesses causing an error message to display on the Grid. Backported to: 19.2.27.0;19.3.3.0 Reviewed by: Chris

--- OpenDental/Forms/FormScheduledProcesses.designer.cs
+++ OpenDental/Forms/FormScheduledProcesses.designer.cs
@@ -66,7 +66,7 @@ namespace OpenDental{
 			this.gridMain.Title = null;
 			this.gridMain.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
 			this.gridMain.TitleHeight = 18;
-			this.gridMain.TranslationName = null;
+			this.gridMain.TranslationName = "TableScheduledProcess";
 			this.gridMain.CellDoubleClick += new OpenDental.UI.ODGridClickEventHandler(this.GridMain_CellDoubleClick);
 			// 
 			// butClose
