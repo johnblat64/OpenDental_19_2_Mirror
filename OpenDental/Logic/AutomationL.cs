07be22fcbb13831fab860991570b17639fe7e0d1
saul <saul@098d0599-7f2b-0410-95a5-ad30ac85393a>
2019-09-27 00:40:09 +0000

    B16989 - Automation, create appointment trigger sometimes did not associate procedures correctly. Backported to: 19.2.29.0;19.3.6.0 Reviewed by: Chris

--- OpenDental/Logic/AutomationL.cs
+++ OpenDental/Logic/AutomationL.cs
@@ -159,6 +159,9 @@ namespace OpenDental {
 						AppointmentType aptTypeCur=AppointmentTypes.GetFirstOrDefault(x => x.AppointmentTypeNum==aptNew.AppointmentTypeNum);
 						if(aptTypeCur!=null) {
 							aptNew.ColorOverride=aptTypeCur.AppointmentTypeColor;
+							aptNew.Pattern=aptTypeCur.Pattern;
+							List<Procedure> listProcs=Appointments.ApptTypeMissingProcHelper(aptNew,aptTypeCur,new List<Procedure>());
+							Procedures.UpdateAptNums(listProcs.Select(x => x.ProcNum).ToList(),aptNew.AptNum,aptNew.AptStatus==ApptStatus.Planned);
 						}
 						Appointments.Update(aptNew,aptOld);//Appointments S-Class handles Signalods
 						continue;
