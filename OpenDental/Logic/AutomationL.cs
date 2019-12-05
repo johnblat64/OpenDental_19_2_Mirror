87f9fa4d15bcec74c94d3ea345b7df1381919b3c
devinf <devinf@098d0599-7f2b-0410-95a5-ad30ac85393a>
2019-12-05 17:46:57 +0000

    B18292 - Appointment Types, time pattern for procedures weren't being used Backported to: 19.2.49.0;19.3.30.0 Reviewed by: Jordan

--- OpenDental/Logic/AutomationL.cs
+++ OpenDental/Logic/AutomationL.cs
@@ -159,10 +159,7 @@ namespace OpenDental {
 						AppointmentType aptTypeCur=AppointmentTypes.GetFirstOrDefault(x => x.AppointmentTypeNum==aptNew.AppointmentTypeNum);
 						if(aptTypeCur!=null) {
 							aptNew.ColorOverride=aptTypeCur.AppointmentTypeColor;
-							aptNew.Pattern=aptTypeCur.Pattern;
-							if (aptNew.Pattern=="") {
-								aptNew.Pattern="/XXXX/";
-							}
+							aptNew.Pattern=AppointmentTypes.GetTimePatternForAppointmentType(aptTypeCur);
 							List<Procedure> listProcs=Appointments.ApptTypeMissingProcHelper(aptNew,aptTypeCur,new List<Procedure>());
 							Procedures.UpdateAptNums(listProcs.Select(x => x.ProcNum).ToList(),aptNew.AptNum,aptNew.AptStatus==ApptStatus.Planned);
 						}
